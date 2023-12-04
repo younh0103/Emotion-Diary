c7d38405d08364707ca5bbc7b706cef4e7180dd0
younh0103 <gkdld1004@gmail.com>
2023-12-04 14:06:31 +0900

    기초공사 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import './App.css';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+import Home from './pages/Home';
+import New from './pages/New';
+import Edit from './pages/Edit';
+import Diary from './pages/Diary';
+
+// Components
+import MyButton from './componets/MyButton';
+import MyHeader from './componets/MyHeader';
 
 function App() {
+  
   return (
     <BrowserRouter>
       <div className="App">
+        <MyHeader headText={"App"} 
+          leftChild={
+            <MyButton text={'왼쪽 버튼'} onClick={() => alert('왼쪽 클릭 !')} />
+          } 
+          rightChild={
+            <MyButton text={'오른쪽 버튼'} onClick={() => alert('오른쪽 클릭 !')} />
+          } />
         <h2>App.js</h2>
+
+        <MyButton text={'버튼'} onClick={() => alert("버튼 클릭")} type={"positive"}/>
+        <MyButton text={'버튼'} onClick={() => alert("버튼 클릭")} type={"negative"}/>
+        <MyButton text={'버튼'} onClick={() => alert("버튼 클릭")} type={"default"}/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new" element={<New />} />
+          <Route path="/edit" element={<Edit />} />
+          <Route path="/diary/:id" element={<Diary />} />
+        </Routes>
       </div>
     </BrowserRouter>
   );
--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,8 +1,22 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
 const Edit = () => {
+    const navigate = useNavigate();
+    const [seachParams, setSearchParams] = useSearchParams();
+
+    const id = seachParams.get('id');
+    console.log('id : ', id);
+
+    const mode = seachParams.get('mode');
+    console.log('mode : ', mode);
+
     return (
         <div>
             <h1>Edit</h1>
             <p>이 곳은 일기 수정 페이지입니다.</p>
+            <button onClick={() => setSearchParams({who: "winterlood"})}>QS 바꾸기</button>
+            <button onClick={() => {navigate("/home");}}>HOME으로 가기</button>
+            <button onClick={() => {navigate(-1);}}>뒤로 가기</button>
         </div>
     );
 };
